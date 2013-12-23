705031765e5b052a4b38b04efb99c5626da13e46
Pieter Kelchtermans <pieter.kelchtermans@ugent.be>
2013-12-23 02:45:13 +0100

    removed Norvigs types in Go they now only introduce extra work

--- scm.go
+++ scm.go
@@ -27,30 +27,30 @@ func main() {
 
 func eval(expression scmo, en *env) (value scmo) {
 	switch e := expression.(type) {
-	case number:
+	case float64:
 		value = e
-	case symbol:
+	case string:
 		value = en.Find(e).vars[e]
 	case []scmo:
 		switch e[0] {
-		case symbol("quote"):
+		case "quote":
 			value = e[1]
-		case symbol("if"):
+		case "if":
 			if eval(e[1], en).(bool) {
 				value = eval(e[2], en)
 			} else {
 				value = eval(e[3], en)
 			}
-		case symbol("set!"):
-			v := e[1].(symbol)
+		case "set!":
+			v := e[1].(string)
 			en.Find(v).vars[v] = eval(e[2], en)
 			value = "ok"
-		case symbol("define"):
-			en.vars[e[1].(symbol)] = eval(e[2], en)
+		case "define":
+			en.vars[e[1].(string)] = eval(e[2], en)
 			value = "ok"
-		case symbol("lambda"):
+		case "lambda":
 			value = proc{e[1], e[2], en}
-		case symbol("begin"):
+		case "begin":
 			for _, i := range e[1:] {
 				value = eval(i, en)
 			}
@@ -77,10 +77,10 @@ func apply(procedure scmo, args []scmo) (value scmo) {
 		switch params := p.params.(type) {
 		case []scmo:
 			for i, param := range params {
-				en.vars[param.(symbol)] = args[i]
+				en.vars[param.(string)] = args[i]
 			}
 		default:
-			en.vars[params.(symbol)] = args
+			en.vars[params.(string)] = args
 		}
 		value = eval(p.body, en)
 	default:
@@ -98,13 +98,13 @@ type proc struct {
  Environments
 */
 
-type vars map[symbol]scmo
+type vars map[string]scmo
 type env struct {
 	vars
 	outer *env
 }
 
-func (e *env) Find(s symbol) *env {
+func (e *env) Find(s string) *env {
 	if _, ok := e.vars[s]; ok {
 		return e
 	} else {
@@ -121,52 +121,52 @@ var globalenv env
 func init() {
 	globalenv = env{
 		vars{ //aka an incomplete set of compiled-in functions
-			symbol("#t"): true,
-			symbol("#f"): false,
-			symbol("+"): func(a ...scmo) scmo {
-				v := a[0].(number)
+			"#t": true,
+			"#f": false,
+			"+": func(a ...scmo) scmo {
+				v := a[0].(float64)
 				for _, i := range a[1:] {
-					v += i.(number)
+					v += i.(float64)
 				}
 				return v
 			},
-			symbol("-"): func(a ...scmo) scmo {
-				v := a[0].(number)
+			"-": func(a ...scmo) scmo {
+				v := a[0].(float64)
 				for _, i := range a[1:] {
-					v -= i.(number)
+					v -= i.(float64)
 				}
 				return v
 			},
-			symbol("*"): func(a ...scmo) scmo {
-				v := a[0].(number)
+			"*": func(a ...scmo) scmo {
+				v := a[0].(float64)
 				for _, i := range a[1:] {
-					v *= i.(number)
+					v *= i.(float64)
 				}
 				return v
 			},
-			symbol("/"): func(a ...scmo) scmo {
-				v := a[0].(number)
+			"/": func(a ...scmo) scmo {
+				v := a[0].(float64)
 				for _, i := range a[1:] {
-					v /= i.(number)
+					v /= i.(float64)
 				}
 				return v
 			},
-			symbol("<="): func(a ...scmo) scmo {
-				return a[0].(number) <= a[1].(number)
+			"<=": func(a ...scmo) scmo {
+				return a[0].(float64) <= a[1].(float64)
 			},
-			symbol("equal?"): func(a ...scmo) scmo {
+			"equal?": func(a ...scmo) scmo {
 				return a[0] == a[1]
 			},
-			symbol("cons"): func(a ...scmo) scmo {
+			"cons": func(a ...scmo) scmo {
 				return []scmo{a[0], a[1]}
 			},
-			symbol("car"): func(a ...scmo) scmo {
+			"car": func(a ...scmo) scmo {
 				return a[0].([]scmo)[0]
 			},
-			symbol("cdr"): func(a ...scmo) scmo {
+			"cdr": func(a ...scmo) scmo {
 				return a[0].([]scmo)[1:]
 			},
-			symbol("list"): eval(read(
+			"list": eval(read(
 				"(lambda z z)"),
 				&globalenv),
 		},
@@ -176,9 +176,9 @@ func init() {
 /*
  Parsing
 */
-type scmo interface{} //scheme objects are e.g. symbols, numbers, expressions, procedures, lists, ...
-type symbol string     //symbols are golang strings
-type number float64    //constant numbers float64
+
+//scheme objects (scmos) are e.g. symbols, numbers, expressions, procedures, lists, ...
+type scmo interface{}
 
 func read(s string) (expression scmo) {
 	tokens := tokenize(s)
@@ -206,9 +206,9 @@ func readFrom(tokens *[]string) (expression scmo) {
 		return nil
 	default: //an atom occurs
 		if f, err := strconv.ParseFloat(token, 64); err == nil {
-			return number(f) //numbers become float64
+			return f //numbers become float64
 		} else {
-			return symbol(token) //others stay string
+			return token //others stay string
 		}
 	}
 }
@@ -244,7 +244,7 @@ func Repl() {
 		fmt.Print("> ")
 		if input, err := reader.ReadString('\n'); err == nil {
 			ans := eval(read(input[:len(input)-1]), &globalenv)
-			globalenv.vars[symbol("ans")] = ans
+			globalenv.vars["ans"] = ans
 			fmt.Println("==>", String(ans))
 		} else {
 			fmt.Println("Bye.")
